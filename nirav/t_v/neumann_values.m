f83007d39f5d1da2832f088a3cd326fd2085e51c
Nirav Shah <snirav@sissa.it>
2019-10-27 01:35:40 +0200

    convergence_code_added

--- nirav/t_v/neumann_values.m
+++ nirav/t_v/neumann_values.m
@@ -3,21 +3,21 @@ function [ res ] = neumann_values(glob,params,paramsP)
 %   Detailed explanation goes here
 
 %% analytical example
-% if glob(1) > 1 - eps
-%     res = [0 0]';
-% else
-%     disp('Not on Neumann boundary')
-% end
+if glob(1) > 1 - eps
+    res = [0 0]';
+else
+    disp('Not on Neumann boundary')
+end
 
 %% benchmark problem lid driven cavity
 % error('No Neumann boundary should exist here');
 
 %% For actual case
-res = zeros(2,1);
-if glob(1)>(1-eps)
-    res = [0 0]';
-else
-    disp('Not on neumann boundary');
-    %error('Not on neumann boundary');
-end
+% res = zeros(2,1);
+% if glob(1)>(1-eps)
+%     res = [0 0]';
+% else
+%     disp('Not on neumann boundary');
+%     %error('Not on neumann boundary');
+% end
 end
